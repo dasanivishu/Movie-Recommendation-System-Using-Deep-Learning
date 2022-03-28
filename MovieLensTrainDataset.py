d00a707b5772829a4f47593a4955dd6946f7a305
Vishal Shyam Dasani <50577261+dasanivishu@users.noreply.github.com>
2022-03-29 02:12:49 +0530

    Create MovieLensTrainDataset.py

---
+++ MovieLensTrainDataset.py
@@ -0,0 +1,41 @@
+import torch
+from torch.utils.data import Dataset
+
+#Below we will just define a pytorch dataset to faciliate training
+#The class below simply encapsulates our dataset in required format into a PyTorch Dataset class.
+class MovieLensTrainDataset(Dataset):
+    """MovieLens PyTorch Dataset for Training
+    
+    Args:
+        ratings (pd.DataFrame): Dataframe containing the movie ratings
+        all_movieIds (list): List containing all movieIds
+    
+    """
+
+    def __init__(self, ratings, all_movieIds):
+        self.users, self.items, self.labels = self.get_dataset(ratings, all_movieIds)
+
+    def __len__(self):
+        return len(self.users)
+  
+    def __getitem__(self, idx):
+        return self.users[idx], self.items[idx], self.labels[idx]
+
+    def get_dataset(self, ratings, all_movieIds):
+        users, items, labels = [], [], []
+        user_item_set = set(zip(ratings['userId'], ratings['movieId']))
+
+        num_negatives = 4
+        for u, i in user_item_set:
+            users.append(u)
+            items.append(i)
+            labels.append(1)
+            for _ in range(num_negatives):
+                negative_item = np.random.choice(all_movieIds)
+                while (u, negative_item) in user_item_set:
+                    negative_item = np.random.choice(all_movieIds)
+                users.append(u)
+                items.append(negative_item)
+                labels.append(0)
+
+        return torch.tensor(users), torch.tensor(items), torch.tensor(labels)
